904bb655bbd3ce1773c306026bdd1172be81569d
fanwenyao <418452307@qq.com>
2020-07-26 22:00:46 +0800

    heap sort

---
+++ 01-Sorting-Basic/07-Heap-Sort/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+#include "sort_test_helper.h"
+
+#define left(i) (2 * i + 1)
+#define right(i) (2 * i)
+
+void max_heapify(int arr[], int size, int i) {
+    int l = left(i);
+    int r = right(i);
+    int largest = i;
+    if (l < size && arr[l] > arr[largest]) {
+        largest = l;
+    }
+    if (r < size && arr[r] > arr[largest]) {
+        largest = r;
+    }
+    if (largest != i) {
+        std::swap(arr[i], arr[largest]);
+        max_heapify(arr, size, largest);
+    }
+}
+
+void build_max_heap(int arr[], int size) {
+    for (int i = size / 2 - 1; i >=0; i--) {
+       max_heapify(arr, size, i); 
+    }
+}
+
+void heap_sort(int arr[], int size) {
+    build_max_heap(arr, size);
+    for (int i = size - 1 ; i >= 1; i--) {
+        std::swap(arr[0], arr[i]);
+        max_heapify(arr, i, 0); 
+    }
+}
+
+int main() {
+    int size = 1000000;
+    int* arr = generate_random_array(size, 0, 1000000);
+    test_sort("heap sort", heap_sort, arr, size);
+    delete [] arr;
+    return 0;
+}
+
