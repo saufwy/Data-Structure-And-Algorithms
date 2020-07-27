1296c2d9375fc87b1c09b7c1b78144a718e384c8
fanwenyao <418452307@qq.com>
2020-07-27 09:54:30 -0700

    use shift_down implement heap sort

--- 01-Sorting-Basic/07-Heap-Sort/main.cpp
+++ 01-Sorting-Basic/07-Heap-Sort/main.cpp
@@ -24,6 +24,25 @@ void max_heapify(int arr[], int size, int i) {
     }
 }
 
+// shift down
+void max_heapify_non_recursion(int arr[], int size, int i) {
+    do { 
+        int l = left(i);
+        int r = right(i);
+        int largest = i;
+        if (l < size && arr[l] > arr[largest]) {
+            largest = l;
+        }
+        if (r < size && arr[r] > arr[largest]) {
+            largest = r;
+        }
+        if (largest != i) {
+            std::swap(arr[i], arr[largest]);
+            i = largest;
+        }
+    } while(true);
+}
+
 void build_max_heap(int arr[], int size) {
     for (int i = size / 2 - 1; i >=0; i--) {
        max_heapify(arr, size, i); 
@@ -39,18 +58,24 @@ void heap_sort(int arr[], int size) {
 }
 
 int main() {
-    // int size = 1000000;
-    // int* arr = generate_random_array(size, 0, 1000000);
-    // test_sort("heap sort", heap_sort, arr, size);
-    // delete [] arr;
+    int size = 1000000;
+    int* arr = generate_random_array(size, 0, 1000000);
+    test_sort("heap sort", heap_sort, arr, size);
+    delete [] arr;
 
-    int size = 20;
+    size = 20;
     srand(time(nullptr));
     MaxHeap<int> max_heap(100);
     for (int i = 0; i < size; i++) {
         max_heap.insert(rand() % 100);
     }
     max_heap.print();
+    std::cout << "extract item form max heap" << std::endl;
+    for (int i = 0; i < size; i++) {
+        int item;
+        max_heap.extract_max(item);
+        std::cout << item << std::endl;
+    }
     return 0;
 }
 
--- 01-Sorting-Basic/07-Heap-Sort/max_heap.h
+++ 01-Sorting-Basic/07-Heap-Sort/max_heap.h
@@ -8,6 +8,19 @@ public:
         _count = 0;
         _capacity = capacity;
     }
+
+    MaxHeap(Item arr[], int n) {
+        _data = new Item[n + 1];
+        _capacity = n;
+        _count = n;
+        for (int i = 0; i < n; i++) {
+            _data[i + 1] = arr[i];
+        }
+        for (int i = _count / 2; i >= 1; i--) {
+            shift_down(i);
+        }
+    }
+
     int size() {
         return _count;
     }
@@ -24,6 +37,17 @@ public:
        shift_up(_count);
     }
 
+    bool extract_max(Item& item) {
+        if (_count < 1) {
+            return false;
+        }
+        item = _data[1];
+        _data[1] = _data[_count--];
+        if (_count > 1) {
+            shift_down(1);
+        }
+    }
+
     void print() const {
         for (int i = 1; i <= _count; i++) {
             std::cout << _data[i] << " ";
@@ -37,6 +61,27 @@ private:
             std::swap(_data[k/2], _data[k]);
             k /= 2;
         }
+    }
+
+    void shift_down(int k) {
+        do {
+            int largest = k;
+            int l = 2 * k;
+            int r = 2 * k + 1;
+            if (l <= _count && _data[l] > _data[largest]) {
+                largest = l;
+            }
+
+            if (r <= _count && _data[r] > _data[largest]) {
+                largest = r;
+            }
+            if (largest != k) {
+                std::swap(_data[largest], _data[k]);
+                k = largest;
+            } else {
+                break;
+            }
+        } while (true);
     }
 
 private:    
