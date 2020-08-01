93b4917fccae97e529c3cf2edc6bca1de2afa534
fanwenyao <418452307@qq.com>
2020-08-01 08:33:28 -0700

    contain and serach

--- 02-Binary-Search-Tree/02-Binary-Search-Basics/main.cpp
+++ 02-Binary-Search-Tree/02-Binary-Search-Basics/main.cpp
@@ -40,6 +40,14 @@ public:
         _root = _insert(_root, key, value);
     }
 
+    bool contain(Key key) const {
+        return _contain(_root, key);
+    }
+
+    Value* search(Key key) {
+        return _search(_root, key);
+    }
+
 protected:
     Node* _insert(Node* node, Key key, Value value) {
         if (node == nullptr) {
@@ -50,15 +58,51 @@ protected:
         } else if (node->key > key) {
             node->left = _insert(node->left, key, value);
         } else {
-            node->_insert(node->right, key, value);
+            node->right = _insert(node->right, key, value);
         }
         return node;
+    }
+
+    bool _contain(Node* node, Key key) const {
+        if (node == nullptr) {
+            return false;
+        }
+
+        if (node->key == key) {
+            return true;
+        } else if (node->key < key) {
+            return _contain(node->right, key);
+        } else { // node->key > key
+            return _contain(node->left, key);
+        }
+    }
+
+    Value* _search(Node* node, Key key) {
+        if (node == nullptr) {
+            return nullptr;
+        }
+
+        if (node->key == key) {
+            return &node->value;
+        } else if (node->key < key) {
+            return _search(node->right, key);
+        } else {
+            return _search(node->left, key);
+        }
     }
 
 };
 
 int main() {
-    BST<std::string,int> t;
+    BST<std::string,int> bst;
+    bst.insert("1", 100);
+    bst.insert("2", 101);
+    bst.insert("3", 102);
+    bst.insert("4", 103);
+    bst.insert("5", 104);
+    if (bst.contain("1")) {
+        std::cout << *bst.search("5") << std::endl;
+    }
     return 0;
 }
 
